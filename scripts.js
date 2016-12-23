4fabbc6902474996824d5a40366ee6ce7bb2cb30
Marcus Sakoda <Jubhub@yahool.com>
2016-12-23 11:42:45 -0800

    fixed empty search box problem

--- scripts.js
+++ scripts.js
@@ -4,16 +4,28 @@ $(document).ready(function(){
 // &APPID=c93d0e55bfcff12da858b54456ab5f3d
 
 
-});
-
   $('#button').click(function(){
-    // $('#submit').submit(function(){
-    //   return false;
-
+    console.log("you clicked search weather")
+    console.log($('#input').val());
+  //
+  //
+    if ($('#input').val()==="type city here" || $('#input').val().length === 0){
+      console.log("you didn't enter a city");
+    }
+    // if ($('#input').val()==="type city here" || ('#input').val()== false){
+    //   console.log("you didn't enter a city");
+    // }
     $.get('http://api.openweathermap.org/data/2.5/weather?q='+$("#input").val()+'&APPID=c93d0e55bfcff12da858b54456ab5f3d',function(rem){
       console.log(rem);
       $('p').html("<p>Temperature: "+Math.floor(rem.main.temp*(9/5)-459.67)+"</p>");
 
     },'json');
+  //
+  //
+  //   // $.get('http://api.openweathermap.org/data/2.5/weather?q='+$('input').attr('value')+'&APPID=c93d0e55bfcff12da858b54456ab5f3d',function(rem){
+  //   //   console.log(rem);
+  //   //   $('p').append(Math.floor(rem.main.temp*(9/5)-459.67));
+  //   //
+  //   // },'json');
   }); //end of button click
 }); //end of document ready
