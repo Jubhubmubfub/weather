ac4cc9df6556bdb0e5f9bceaa75a3da7fa7c8a24
Marcus Sakoda <Jubhub@yahool.com>
2016-12-23 11:15:01 -0800

    finished code and validated html

--- scripts.js
+++ scripts.js
@@ -2,32 +2,18 @@ $(document).ready(function(){
 
 // my key for weather api
 // &APPID=c93d0e55bfcff12da858b54456ab5f3d
-console.log("hello world");
-$('#button').click(function(event){
-  event.stopPropagation();
+
+$('#input').submit(function(){
+  return false;
+
 });
 
-$('#wrapper').on('click','#button',function(){
-  
-});
+  $('#button').click(function(){
 
+    $.get('http://api.openweathermap.org/data/2.5/weather?q='+$("#input").val()+'&APPID=c93d0e55bfcff12da858b54456ab5f3d',function(rem){
+      console.log(rem);
+      $('p').html("<p>Temperature: "+Math.floor(rem.main.temp*(9/5)-459.67)+"</p>");
 
-  // $('#button').click(function(){
-  //   console.log("you clicked search weather")
-  //   console.log($('#input').attr('value'));
-  //
-  //
-  //   $.get('http://api.openweathermap.org/data/2.5/weather?q='+$("#input").attr("value")+'&APPID=c93d0e55bfcff12da858b54456ab5f3d',function(rem){
-  //     console.log(rem);
-  //     $('p').html("<p>Temperature: "+Math.floor(rem.main.temp*(9/5)-459.67)+"</p>");
-  //
-  //   },'json');
-  //
-  //
-  //   // $.get('http://api.openweathermap.org/data/2.5/weather?q='+$('input').attr('value')+'&APPID=c93d0e55bfcff12da858b54456ab5f3d',function(rem){
-  //   //   console.log(rem);
-  //   //   $('p').append(Math.floor(rem.main.temp*(9/5)-459.67));
-  //   //
-  //   // },'json');
-  // }); //end of button click
+    },'json');
+  }); //end of button click
 }); //end of document ready
