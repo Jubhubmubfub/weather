64aa6d267ce025d2f84807a1b2043fc070018589
Marcus Sakoda <Jubhub@yahool.com>
2016-12-23 11:21:49 -0800

    broke some code

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 // my key for weather api
 // &APPID=c93d0e55bfcff12da858b54456ab5f3d
 
-$('#input').submit(function(){
-  return false;
 
 });
 
   $('#button').click(function(){
+    // $('#submit').submit(function(){
+    //   return false;
 
     $.get('http://api.openweathermap.org/data/2.5/weather?q='+$("#input").val()+'&APPID=c93d0e55bfcff12da858b54456ab5f3d',function(rem){
       console.log(rem);
